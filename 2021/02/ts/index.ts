d912777db6d1d45797ac097c88b78ff37eccca2f
Marat Amirov <marat.amirov@vkpay.com>
2021-12-07 19:07:30 +0300

    day 02: second question answer

--- 2021/02/ts/index.ts
+++ 2021/02/ts/index.ts
@@ -26,6 +26,31 @@ const getPosition = (d: string[][]): number[] => {
   return result;
 };
 
+const getPositionWithAim = (d: string[][]): number[] => {
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+  d.forEach(([direction, steps]) => {
+    const stepsCount = Number(steps);
+    switch(direction) {
+      case 'forward':
+        horizontal = horizontal + stepsCount;
+        depth = depth + (aim * stepsCount);
+        break;
+      case 'down':
+        aim = aim + stepsCount;
+        break;
+      case 'up':
+        aim = aim - stepsCount;
+        break;
+      default:
+        break;
+    }
+  });
+  return [horizontal, depth];
+};
+
 const multiply = (d: number[]) => d.reduce((acc, cur) => acc * cur, 1);
 
 console.log('first answer:', Math.abs(multiply(getPosition(data))));
+console.log('second answer:', Math.abs(multiply(getPositionWithAim(data))));
