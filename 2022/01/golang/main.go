1a1e49bb1b279f8541491c0ed9601bde8c9c4499
Marat Amirov <m.amirov@vk.team>
2022-12-15 18:42:03 +0300

    Day 01 Part 2 Solution

--- 2022/01/golang/main.go
+++ 2022/01/golang/main.go
@@ -7,35 +7,55 @@ import (
 	"strconv"
 )
 
+func getTotalCalories(caloriesArr *[3]int) (total int) {
+	total = 0
+	for _, calories := range (*caloriesArr) {
+		total += calories
+	}
+	return
+}
+
+func compareCalories(caloriesArr *[3]int, comparable int) {
+	if (comparable > (*caloriesArr)[0]) {
+		(*caloriesArr)[2] = (*caloriesArr)[1]
+		(*caloriesArr)[1] = (*caloriesArr)[0]
+		(*caloriesArr)[0] = comparable
+	} else if (comparable > (*caloriesArr)[1] && comparable != (*caloriesArr)[0]) {
+		(*caloriesArr)[2] = (*caloriesArr)[1]
+		(*caloriesArr)[1] = comparable
+	} else if (comparable > (*caloriesArr)[2] && comparable != (*caloriesArr)[1]) {
+		(*caloriesArr)[2] = comparable
+	}
+}
+
 func main() {
 	data, err := os.Open("../input.txt")
 	if (err != nil) {
 		os.Exit(1)
 	}
 
-	var m int = 0
-	var b int = 0
+	buffer := 0
+	topCalories := [3]int{0, 0, 0}
 
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
-			b = 0
+			buffer = 0
 			continue
 		}
-		n, nerr := strconv.Atoi(line)
-		if nerr != nil {
+		calories, caloriesErr := strconv.Atoi(line)
+		if caloriesErr != nil {
 			os.Exit(1)
 		}
-		b = b + n
-		if b > m {
-			m = b
-		}
+		buffer += calories
+		compareCalories(&topCalories, buffer)
 	}
 
 	if scanner.Err() != nil {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Part 1 Solution: %d\n", m)
+	fmt.Printf("Part 1 Solution: %d\n", topCalories[0])
+	fmt.Printf("Part 2 Solution: %d\n", getTotalCalories(&topCalories))
 }
